87eeddbd2158a2dd0c7f46240668462a538eb22e
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2021-01-27 14:24:19 -0500

    round trip (not working)

--- src/pynwb/tests/test_ellipse_eye_tracking.py
+++ src/pynwb/tests/test_ellipse_eye_tracking.py
@@ -2,7 +2,7 @@ import datetime
 import numpy as np
 
 from pynwb import NWBHDF5IO, NWBFile
-from pynwb.testing import TestCase, remove_test_file, AcquisitionH5IOMixin
+from pynwb.testing import TestCase, remove_test_file
 
 from ndx_ellipse_eye_tracking import EllipseEyeTracking, EllipseSeries
 from ndx_events import Events
@@ -18,14 +18,18 @@ def set_up_nwbfile():
     return nwbfile
 
 
-class TestTetrodeSeriesConstructor(TestCase):
+class TestEllipseEyeTracking(TestCase):
 
     def setUp(self):
-        """Set up an NWB file. Necessary because TetrodeSeries requires references to electrodes."""
+        """Set up an NWB file."""
         self.nwbfile = set_up_nwbfile()
+        self.path = 'test.nwb'
+
+    def tearDown(self):
+        remove_test_file(self.path)
 
     def test_constructor(self):
-        """Test that the constructor for TetrodeSeries sets values as expected."""
+        """Test that the constructors."""
 
         eye_tracking = EllipseSeries(
             name='eye_tracking',
@@ -63,51 +67,23 @@ class TestTetrodeSeriesConstructor(TestCase):
         likely_blink = Events(timestamps=[1.0, 5.0, 6.0], name='likely_blink',
                               description='blinks')
 
-        EllipseEyeTracking(
+        ellipse_eye_tracking = EllipseEyeTracking(
             eye_tracking=eye_tracking,
             pupil_tracking=pupil_tracking,
             corneal_reflection_tracking=corneal_reflection_tracking,
             likely_blink=likely_blink
         )
 
-'''
-class TestTetrodeSeriesRoundtrip(TestCase):
-    """Simple roundtrip test for TetrodeSeries."""
-
-    def setUp(self):
-        self.nwbfile = set_up_nwbfile()
-        self.path = 'test.nwb'
-
-    def tearDown(self):
-        remove_test_file(self.path)
+        return ellipse_eye_tracking
 
     def test_roundtrip(self):
-        """
-        Add a TetrodeSeries to an NWBFile, write it to file, read the file, and test that the TetrodeSeries from the
-        file matches the original TetrodeSeries.
-        """
-        pass
 
-        self.nwbfile.add_acquisition()
+        ellipse_eye_tracking = self.test_constructor()
+        self.nwbfile.add_acquisition(ellipse_eye_tracking)
 
         with NWBHDF5IO(self.path, mode='w') as io:
             io.write(self.nwbfile)
 
         with NWBHDF5IO(self.path, mode='r', load_namespaces=True) as io:
             read_nwbfile = io.read()
-            self.assertContainerEqual(tetrode_series, read_nwbfile.acquisition['TetrodeSeries'])
-'''
-
-'''
-class TestTetrodeSeriesRoundtripPyNWB(AcquisitionH5IOMixin, TestCase):
-    """Complex, more complete roundtrip test for TetrodeSeries using pynwb.testing infrastructure."""
-
-    def setUpContainer(self):
-        """ Return the test TetrodeSeries to read/write """
-        
-        return tetrode_series
-
-    def addContainer(self, nwbfile):
-        """Add the test TetrodeSeries and related objects to the given NWBFile."""
-        nwbfile.add_acquisition(self.container)
-'''
+            self.assertContainerEqual(ellipse_eye_tracking, read_nwbfile.acquisition['EllipseEyeTracking'])
